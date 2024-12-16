52659f8aa99b09b2d4cfb01ddecd449d5d7604ee
samfran <siamack2pazjeta@gmail.com>
2024-12-15 21:14:26 -0800

    add listagem de contatos salvos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,80 @@
-import {View, Text} from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
+import { useFocusEffect } from "expo-router";
+import { useContatosRepository } from "@/data/ContatosRepository";
+import { useCallback, useState } from "react";
+import { Contato } from "@/data/types";
 
 export default function Index() {
+  const [contatos, setContatos] = useState<Contato[]>([]);
+  const repository = useContatosRepository(); // create a single instance of the repository
+
+  async function fetchContatos() {
+    const fetchedContatos = await repository.findMany();
+    setContatos(fetchedContatos);
+    console.log(fetchedContatos);
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchContatos().catch((error) => console.error(error));
+    }, []),
+  );
+
+
+  function renderItem({ item }: { item: Contato }) {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Edit app/index.tsx to edit this screen.</Text>
-        </View>
+      <View style={styles.contactItem}>
+        <TouchableOpacity>
+          <Text style={styles.nome}>{item.nome}</Text>
+          <Text style={styles.telefone}>{item.telefone}</Text>
+          <Text style={styles.email}>{item.email}</Text>
+        </TouchableOpacity>
+      </View>
     );
+  }
+
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <FlatList data={contatos} renderItem={renderItem} />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#000",
+  },
+  contactItem: {
+    padding: 15,
+    borderBottomWidth: 10,
+    borderBottomColor: "#ccc",
+  },
+  nome: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  telefone: {
+    fontSize: 16,
+    color: "#555",
+  },
+  email: {
+    fontSize: 14,
+    color: "#888",
+  },
+});
